7537a0437a20e7151c3c428fc23614cfa3bac233
Jakob Lingel <jlingel68@gmail.com>
2022-07-10 21:04:50 +0200

    Working pytesseract with images and pdf

--- scanPages.py
+++ scanPages.py
@@ -1,8 +1,26 @@
 # scans pages for numbers in a specified format
+import os
 import pytesseract
+import cv2
+from pdf2image import convert_from_path
+from PIL import Image
 
-def ocr_core(file):
-    text = pytesseract.image_to_string(file, lang='ger')
-    return text
+# filePath = "/home/jakob/OneDrive/HochschuleAA/bachelor-2/WS21/AuD2/Altklausuren/Algo Prüfung 21-22.pdf"
+# filePath = "/home/jakob/OneDrive/HochschuleAA/bachelor-2/SS22/Rechnernetze/Altklausuren/Lingel_Korr_2.pdf"
+# filePath = "/home/jakob/OneDrive/HochschuleAA/bachelor-2/SS22/Rechnernetze/Altklausuren/Ostermann_Korrektur.pdf"
+# filePath = "/home/jakob/OneDrive/HochschuleAA/bachelor-2/SS22/Rechnernetze/Aufschrieb.pdf"
+image = "/home/jakob/Pictures/ocr_testdata1.jpg"
 
-print(ocr_core('~/Pictures/Screenshots/2022-02-13_19-38-07_screenshot.png'))
+# use the below for images
+viewedImage = cv2.imread(image)
+string = pytesseract.image_to_string(viewedImage)
+print(string)
+
+try:
+    doc = convert_from_path(filePath)
+    for page_number, page_data in enumerate(doc):
+        txt = pytesseract.image_to_string(page_data, lang="deu").encode("utf-8")
+        print("Page # {} - {}".format(str(page_number),txt))
+except:
+    print("error")
+
